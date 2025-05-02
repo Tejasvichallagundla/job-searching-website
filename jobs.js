c1f147ed65a8ec1a9e519785d802485a70e94dfb
Tejasvi Challagundla <tejasvichallagundla24@gmail.com>
2025-05-02 22:56:10 +0530

    added role and location specification

--- jobs.js
+++ jobs.js
@@ -1,8 +1,10 @@
 
 async function fetchjobfunction(){
     const ApiKey="2e4ee7ec5d4e5d53d81662a4fdad3f71";
-    const ApiId="24ab108e"
-    const jobresponse= await fetch(`http://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=${ApiId}&app_key=${ApiKey}`);
+    const ApiId="24ab108e";
+    const jobresponse= await fetch(`https://api.adzuna.com/v1/api/jobs/in/search/1?app_id=24ab108e&app_key=2e4ee7ec5d4e5d53d81662a4fdad3f71&what=software&where=Hyderabad
+
+`);
     const jobdata= await jobresponse.json();
     console.log(jobdata);
   for(let i=0;i<jobdata.results.length;i++){
@@ -15,3 +17,12 @@ async function fetchjobfunction(){
  
 }
 
+//https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=24ab108e&app_key=2e4ee7ec5d4e5d53d81662a4fdad3f71&what=developer&where=Hyderabad
+//http://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=${ApiId}&app_key=${ApiKey}
+//https://api.adzuna.com/v1/api/jobs/in/categories?app_id=24ab108e&app_key=2e4ee7ec5d4e5d53d81662a4fdad3f71
+//https://api.adzuna.com/v1/api/jobs/in/histogram?app_id=24ab108e&app_key=2e4ee7ec5d4e5d53d81662a4fdad3f71&what=developer&location0=hyderabad
+
+
+
+//https://api.adzuna.com/v1/api/jobs/in/search/1?app_id=24ab108e&app_key=2e4ee7ec5d4e5d53d81662a4fdad3f71&what=JOB_ROLE&where=Hyderabad
+//https://api.adzuna.com/v1/api/jobs/${country}/search/1?app_id=${ApiId}&app_key=${ApiKey}&what=${jobrole}&where=${location}
