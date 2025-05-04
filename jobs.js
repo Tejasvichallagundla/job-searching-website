544ff4bd1b92240633815376b6e83a9766b249f4
Tejasvi Challagundla <tejasvichallagundla24@gmail.com>
2025-05-04 21:46:44 +0530

    updated read more

--- jobs.js
+++ jobs.js
@@ -13,7 +13,9 @@ async function fetchjobfunction(){
     console.log(jobdata);
   for(let i=0;i<jobdata.results.length;i++){
         const jobdetails= document.getElementById("job-details-card");
-       
+        const fullDesc = jobdata.results[i].description;
+        const shortDesc = fullDesc.length > 150 ? fullDesc.substring(0, 150) : fullDesc;
+
         jobdetails.innerHTML +=
         `<div class="job-card">
         <div class="heading">
@@ -25,7 +27,8 @@ async function fetchjobfunction(){
         ${jobdata.results[i].salary_max ? `<h4 class="salary">Max salary: ${jobdata.results[i].salary_max}</h4>` : ''}
         ${jobdata.results[i].contract_time ? `<h4 class="contract-time">Contract Time: ${jobdata.results[i].contract_time}</h4>` : ''}
         
-        <p>${jobdata.results[i].description}</p>
+        <p class="job-desc">${shortDesc}...</p>
+      ${jobdata.results[i].description.length > 150 ? `<span class="read-more" style="color: white; cursor: pointer;" onclick="toggleDescription(this, \`${shortDesc}\`, \`${fullDesc}\`)">Read More</span>` : ''}
         <a href="${jobdata.results[i].redirect_url}"><button class="applynow-button">Apply Now</button></a>
         </div>`
         
@@ -33,6 +36,16 @@ async function fetchjobfunction(){
   };
   
  
+}
+function toggleDescription(elem, shortDesc, fullDesc) {
+  const para = elem.previousElementSibling;
+  if (para.innerText.includes("...")) {
+      para.innerText = fullDesc;
+      elem.innerText = "Show Less";
+  } else {
+      para.innerText = shortDesc + "...";
+      elem.innerText = "Read More";
+  }
 }
 
 //https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=24ab108e&app_key=2e4ee7ec5d4e5d53d81662a4fdad3f71&what=developer&where=Hyderabad
